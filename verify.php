95b5f5805b993501c40a5763d20c0cb3a1a3f694
Anirudh <19.anirudh.sharma@gmail.com>
2015-05-01 10:51:31 +0100

    final

--- verify.php
+++ verify.php
@@ -6,6 +6,7 @@
 <?php
 $registration_failed = false;
 $user_token = $_GET["token"];
+echo ($_GET["token"]);
 
 $query = "SELECT * ";
 $query .= "FROM user_register ";
@@ -14,7 +15,7 @@ $query .= "LIMIT 1";
 	$result = mysqli_query($connection, $query);
 	
 	confirm_query($result);
-	if(mysqli_num_rows($result) == 1){
+	if(mysqli_num_rows($result) > 0){
 				//Success, mark user as logged in
 		$row = mysqli_fetch_assoc($result);
 		$_SESSION["user_id"] = $row["user_id"];
@@ -22,12 +23,13 @@ $query .= "LIMIT 1";
 		$_SESSION["user_email"] = $row["user_email"];
 		$registration_failed = true;
 		//query for changing account status to active
-		$status_query = "UPDATE user_register SET status = 'TRUE' WHERE user_id = '{$row["user_id"]}'";
+		$status_query = "UPDATE user_register SET status = 'TRUE' WHERE user_id = '{$_SESSION["user_id"]}'";
 	$result_status = mysqli_query($connection, $status_query);
 	confirm_query($result_status);		
 		//make query for token deletion 
 		redirect_to("home.php");
 	}else{
+		
 		$registration_failed = false;
 	}
 
@@ -42,7 +44,7 @@ $query .= "LIMIT 1";
 <body>
 <?php
 if(!$registration_failed){
-	echo "Cannot Register";
+	//echo "Cannot Register";
 }
 ?>
 </body>
